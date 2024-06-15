ce462c7d487cb9044c43944d9b45ff80aa17b6bd
Karl David <karldavidh@gmail.com>
2024-06-15 20:08:29 +0200

    updated more code layout and cled it up a bit

--- src/main/java/tech/kdgaming1/irespectyouroptions/applyDefaultOptions.java
+++ src/main/java/tech/kdgaming1/irespectyouroptions/applyDefaultOptions.java
@@ -3,17 +3,39 @@ package tech.kdgaming1.irespectyouroptions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystemException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-    // This method is called when the mod is loaded and is used to apply the default options
-    public class applyDefaultOptions {
-        private static final Logger LOGGER = LogManager.getLogger(iRespectYourOptions.class);
-        public static void applyDefaultOptions(File file, File defaultFile) throws IOException {
+public class applyDefaultOptions {
+
+    private static final Logger LOGGER = LogManager.getLogger(iRespectYourOptions.class);
+
+    public static void apply() {
+        String runDir = iRespectYourOptions.runDir;
+        String configDir = iRespectYourOptions.configDir;
+
+        try {
+            Files.walk(new File(iRespectYourOptions.configDir, "iRespectYourOptions").toPath()).forEach(path -> {
+                File file = path.normalize().toAbsolutePath().normalize().toFile();
+                if (!file.isFile()) return;
+                try {
+                    applyDefaultOptions(new File(configDir, file.getName()), file);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            iRespectYourOptions.hasCopy = true;
+            LOGGER.info("Default options have been successful applied. If you want to override your options bake to the default, delete the iRespectYourOptions.cfg in your config folder or change the value inside it from true to false and save and start the game.");
+        } catch (Exception e) {
+            LOGGER.error("Failed to apply default options.", e);
+        }
+    }
+
+    public static void applyDefaultOptions(File file, File defaultFile) throws IOException {
         try {
             if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
                 throw new IllegalStateException("Could not create directory: " + file.getParentFile().getAbsolutePath());
@@ -32,7 +54,7 @@ import java.nio.file.StandardCopyOption;
         } catch (FileSystemException e) {
             LOGGER.error("File is being used by another process: " + defaultFile.getAbsolutePath(), e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("Failed to apply default options.", e);
         }
     }
 }
--- src/main/java/tech/kdgaming1/irespectyouroptions/iRespectYourOptions.java
+++ src/main/java/tech/kdgaming1/irespectyouroptions/iRespectYourOptions.java
@@ -13,7 +13,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.StandardOpenOption;
 
 
 @Mod(modid = iRespectYourOptions.MOD_ID, version = iRespectYourOptions.VERSION, dependencies = "before:*")
@@ -22,10 +22,11 @@ public class iRespectYourOptions {
     public static final String VERSION = "0.2.3-1.8.9";
     private static final Logger LOGGER = LogManager.getLogger(iRespectYourOptions.class);
 
-    public iRespectYourOptions() {
+    public static String runDir = Paths.get("").toAbsolutePath().toString();
+    public static String configDir = Paths.get(runDir, "config").toString();
+    public static boolean hasCopy;
 
-        String runDir = Paths.get("").toAbsolutePath().toString();
-        String configDir = Paths.get(runDir, "config").toString();
+    public iRespectYourOptions() {
 
         LOGGER.info("Applying default options... (iRespectYourOptions)");
         try {
@@ -33,52 +34,33 @@ public class iRespectYourOptions {
             if (!iRespectYourOptions.exists() && !iRespectYourOptions.mkdirs()) {
                 throw new IllegalStateException("Could not create directory: " + iRespectYourOptions.getAbsolutePath());
             }
-            new File(iRespectYourOptions, "options.txt").createNewFile();
-            new File(iRespectYourOptions, "optionsof.txt").createNewFile();
+            File exampleFile = new File(iRespectYourOptions, "exampleConfig_openForInstructions.txt");
+            if (exampleFile.createNewFile()) {
+                String content = "This is the text content to write into file";
+                Files.write(Paths.get(exampleFile.getPath()), content.getBytes(), StandardOpenOption.APPEND);
+            }
+            File exampleFile2 = new File(iRespectYourOptions, "exampleConfig2_openForInstructions.txt");
+            if (exampleFile.createNewFile()) {
+                String content = "This is the text content to write into file";
+                Files.write(Paths.get(exampleFile.getPath()), content.getBytes(), StandardOpenOption.APPEND);
+            }
             File config = new File(iRespectYourOptions, "config");
             if (!config.exists() && !config.mkdirs()) {
                 LOGGER.info("Remember to put content in to the options.txt and optionsof.txt files in the iRespectYourOptions folder in the config folder.");
                 throw new IllegalStateException("Could not create directory: " + config.getAbsolutePath());
             }
-            
-                // This makes the config file with the info that says if the mod has run before
+
+            // This makes the config file with the info that says if the mod has run before
             File configFile = new File(Loader.instance().getConfigDir(), "iRespectYourOptions.cfg");
             Configuration configuration = new Configuration(configFile);
             configuration.load();
             Property hasCopyProperty = configuration.get("copy", "hasCopy", false, "Set to true to apply default options");
-            boolean hasCopy = hasCopyProperty.getBoolean();
+            hasCopy = hasCopyProperty.getBoolean();
 
-                // To write a massage to the console to say that the default options have already been applied and how you can override them if you want
             if (hasCopy) {
                 LOGGER.info("Default options have already been applied. If you want to override your options bake to the default, delete the iRespectYourOptions.cfg in your config folder or change the value inside it from true to false and save and start the game.");
-            }
-
-                // This is a check to see if the default options have already been applied
-            if (!hasCopy) {
-                // Runs through all the files in the iRespectYourOptions directory and applies the default options
-                Files.walk(iRespectYourOptions.toPath()).forEach(path -> {
-                    File file = path.normalize().toAbsolutePath().normalize().toFile();
-                    if (!file.isFile()) return;
-                    try {
-                        try {
-                            Path configRelative = config.toPath().toAbsolutePath().normalize().relativize(file.toPath().toAbsolutePath().normalize());
-                            if (configRelative.startsWith("iRespectYourOptions"))
-                                throw new IllegalStateException("Illegal default config file: " + file);
-                            applyDefaultOptions.applyDefaultOptions(new File(configDir, configRelative.normalize().toString()), file);
-                        } catch (IllegalArgumentException e) {
-                            System.out.println(iRespectYourOptions.toPath().toAbsolutePath().normalize());
-                            System.out.println(file.toPath().toAbsolutePath().normalize());
-                            System.out.println(iRespectYourOptions.toPath().toAbsolutePath().normalize().relativize(file.toPath().toAbsolutePath().normalize()));
-                            applyDefaultOptions.applyDefaultOptions(new File(runDir, iRespectYourOptions.toPath().toAbsolutePath().normalize().relativize(file.toPath().toAbsolutePath().normalize()).normalize().toString()), file);
-                        }
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-                // Sets the hasCopy property to true and saves the config file
-            hasCopyProperty.set(true);
-            LOGGER.info("Default options have been successful applied. If you want to override your options bake to the default, delete the iRespectYourOptions.cfg in your config folder or change the value inside it from true to false and save and start the game.");
-            configuration.save();
+            } else {
+                applyDefaultOptions.apply();
             }
         } catch (Exception e) {
             LOGGER.error("Failed to apply default options.", e);
